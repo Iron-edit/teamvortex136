b16211a55223ea4e33679a971e2b87cbf7a1487f
Iron-edit <i.ironmax136@gmail.com>
2026-05-02 11:13:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+
+
+const playersData = [
+{
+    name: "Skyzz",
+    role: "Espagnol",
+    img: "https://static-cdn.jtvnw.net/jtv_user_pictures/b1906392-e78e-4196-878d-ee7c930b1d2c-profile_image-70x70.png",
+    twitter: "https://x.com/Skyzzfn___",
+    twitch: "",
+  } ,
+
+  {
+    name: "Besak",
+    role: "Français",
+    img: "https://pbs.twimg.com/profile_images/1915469140036366336/mRhrCPHF_400x400.jpg",
+    twitter: "https://x.com/besakftn",
+    twitch: "",
+  } , 
+
+  {
+    name: "Xsweat",
+    role: "Français",
+    img: "https://static-cdn.jtvnw.net/jtv_user_pictures/80f7da2b-36a1-4683-a929-58bd2447fd0d-profile_image-70x70.png",
+    twitter: "https://x.com/xsweatfn",
+    twitch: "",
+  }
+
+];
+
+const staffData = [
+    {
+      name: "Kiko",
+      role: "CEO",
+      img: "https://media.discordapp.net/attachments/1498359205168545955/1498717313241977032/e1ef38934b82b6c6c8c67871fb09e381.jpg?ex=69f57884&is=69f42704&hm=fe3634bec826fa58a1c2580918dd9ff5ea340a03843ab8b58ce4e713f6f0d4b9&=&format=webp",
+      twitter: "https://x.com/Weykofnr",
+      twitch : "https://www.twitch.tv/kiko3k_", 
+     
+    },
+
+    {
+        name: "Iron",
+        role: "Manager / Aide",
+        img: "https://pbs.twimg.com/profile_images/1912923168311808000/cygPuOlh_400x400.jpg",
+        twitter: "https://x.com/VTX-Iron",
+        twitch: "https://www.twitch.tv/ironftn_"
+      }
+  ];
+
+  function display(id, data, isStaff = false) {
+    const container = document.getElementById(id);
+    if (!container) return;
+  
+    data.forEach(p => {
+  
+      let socials = `
+      
+      <a href="${p.twitter || '#'}" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+      `;
+  
+      // 🔥 PLUS DE RÉSEAUX POUR LE STAFF
+      if (isStaff) {
+        socials += `
+        <a href="${p.twitch|| '#'}" target="_blank"><i class="fa-brands fa-twitch"></i></a>
+    `;
+      }
+  
+      container.innerHTML += `
+        <div class="card">
+          <img src="${p.img}">
+          <h3>${p.name}</h3>
+          <p>${p.role}</p>
+  
+          <div class="card-socials">
+            ${socials}
+          </div>
+        </div>
+      `;
+    });
+  }
+
+  display("players", playersData, false);
+  display("staff", staffData, true);
+
